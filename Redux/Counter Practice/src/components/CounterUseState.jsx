40ef72994b82a36f8a31b3fca68f6cac528db30e
unknown <satyam.kyp.lkr@gmail.com>
2023-04-20 23:15:20 +0530

    'Normal_counter_

---
+++ Redux/Counter Practice/src/components/CounterUseState.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useState } from "react";
+
+const CounterUseState = () => {
+  const [count, setCount] = useState(0);
+
+  const add = () => {
+    setCount(count + 1);
+  };
+  const sub = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <h2> Count : {count} </h2>
+      <button onClick={() => sub()} disabled={count === 0}>-</button>
+      <button onClick={() => add()}> + </button>
+
+      {/* <button onClick={() => setCount(count - 1)} disabled={count === 0}>-</button> */}
+      {/* <button onClick={() => setCount(count + 1)}> + </button> */}
+          
+    </div>
+  );
+};
+
+export default CounterUseState;
