5173e347d91d6e446ff3c9f140801fc10e9612ce
unknown <satyam.kyp.lkr@gmail.com>
2023-04-23 19:18:07 +0530

    Counter_Redux_only

--- Redux/Counter Practice/src/App.js
+++ Redux/Counter Practice/src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import CounterRedux from "./components/CounterRedux";
-import CounterUseState from "./components/CounterUseState";
+
 
 function App() {
   
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <CounterRedux />
-      <CounterUseState/>
+
     </div>
   );
 }
--- Redux/Counter Practice/src/components/CounterRedux.jsx
+++ Redux/Counter Practice/src/components/CounterRedux.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { add, sub } from "../redux/action";
+
 const CounterRedux = () => {
   const count = useSelector((state) => state.count);
+  
   const dispatch = useDispatch();
+
   return (
     <div>
+
       <h1> {count} </h1>
+      
       <button disabled={count === 0} onClick={() => dispatch(sub())}> - </button>
+
       <button onClick={() => dispatch(add())}> +</button>
 
-//      <button onClick={() => setCount(count - 1)} disabled={count === 0}>-</button>
-//      <button onClick={() => setCount(count + 1)}> + </button>
 
     </div>
   );
